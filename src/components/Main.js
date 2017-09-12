6b073adedbcf4702421203e2c9d557eaa2254d8b
Phil <Phil@Cathys-MacBook-Pro.local>
2017-09-12 18:36:55 -0400

    Add in bootstrap cdn

--- src/components/Main.js
+++ src/components/Main.js
@@ -22,7 +22,6 @@ let Main = React.createClass({
         return (
             <div className="container">
                 <div className="btn-group" role="group" aria-label="Basic example">
-                // button group for all 5 cities
                     <Link to="/Chicago"><button className="btn btn-secondary" type="button">Chicago</button></Link>
                     <Link to="/Dc"><button className="btn btn-secondary" type="button">Dc</button></Link>
                     <Link to="/Miami"><button className="btn btn-secondary" type="button">Miami</button></Link>
--- src/index.js
+++ src/index.js
@@ -5,26 +5,6 @@ import ReactDOM from "react-dom";
 import Routes from "./config/routes";
 import { Router, browserHistory } from 'react-router'
 
-// const Main = require("./components/Main");
-// Include the react-router module
-// const router = require("react-router");
-
-
-// Include the Route component for displaying individual routes
-// const Route = router.Route;
-
-// Include the Router component to contain all our Routes
-// Here where we can pass in some configuration as props
-// const Router = router.Router;
-
-// Include the hashHistory prop to handle routing client side without a server
-// https://github.com/ReactTraining/react-router/blob/master/docs/guides/Histories.md#hashhistory
-// const hashHistory = router.hashHistory;
-
-// // Include the IndexRoute (catch-all route)
-// const IndexRoute = router.IndexRoute;
-
-
 console.log(Routes)
 
 // Render content
